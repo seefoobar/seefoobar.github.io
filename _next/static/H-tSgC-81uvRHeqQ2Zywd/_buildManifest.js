cac3722f1bd14b805c189a2d730a7ef0098a98bb
seefoobar <seefoobar@users.noreply.github.com>
2021-12-03 17:24:14 +0000

    deploy: 0ead66fb49ef206779c7cf1cf6e26f386398b16e

---
+++ _next/static/H-tSgC-81uvRHeqQ2Zywd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0b930263f5dbd8bf.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
